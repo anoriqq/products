f78c2db0601de9bbd72ef9ffe9ecae5debc294fb
anoriqq <shota.yoshikawa@anoriqq.com>
2019-11-19 14:42:11 +0900

    feat: packageの追加と調整

--- packages/portfolio/index.js
+++ packages/portfolio/index.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => { res.status(200).send({ ok: true }); });
+app.get('/', (req, res) => {
+  return res.status(200).send({ ok: true, name: 'portfolio' });
+});
 require('http').createServer(app).listen(8000, '0.0.0.0', () => {
   return console.log('Listening on http://0.0.0.0:8000');
 });
