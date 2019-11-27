015caec1566244a740a2e1d8a460b37a75995759
anoriqq <shota.yoshikawa@anoriqq.com>
2019-11-27 22:41:02 +0900

    feat(delay-tweet): decorator化

--- packages/delay-tweet/client/src/App.vue
+++ packages/delay-tweet/client/src/App.vue
@@ -1,3 +1,12 @@
 <template lang="pug">
-  h1 Hello World!
+  h1 {{message}}
 </template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  message = 'This is a data';
+};
+</script>
