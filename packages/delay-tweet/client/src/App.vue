b70ac2fc385f7167d1546c3c0038e8f80a4c1cc7
anoriqq <shota.yoshikawa@anoriqq.com>
2019-11-28 02:10:02 +0900

    feat(delay-tweet): 時間とボタンを追加してみた

--- packages/delay-tweet/client/src/App.vue
+++ packages/delay-tweet/client/src/App.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
-  h1 {{message}}
+  .App
+    h1 {{message}}
+    h3 {{time}}
+    v-btn(
+      class="ma-2"
+      :loading="loading"
+      :disabled="loading"
+      @click="click"
+    ) Tweet
 </template>
 
 <script lang="ts">
@@ -7,6 +15,22 @@ import {Vue, Component} from 'vue-property-decorator';
 
 @Component
 export default class App extends Vue {
-  message = 'This is a data';
+  message: string = 'This is a data';
+  time: Date = new Date();
+  loading: boolean = false;
+
+  mounted(){
+    return this.setTime();
+  };
+
+  setTime(){
+    return setInterval(()=>{
+      return this.time = new Date();
+    }, 100);
+  };
+  click(){
+    this.loading = true;
+    setTimeout(()=>{this.loading = false}, 3000);
+  };
 };
 </script>
