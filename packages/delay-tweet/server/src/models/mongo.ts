55836cf2c73d4d7c8745bb83eea3a61ea6c48c78
anoriqq <shota.yoshikawa@anoriqq.com>
2019-12-08 22:32:14 +0900

    feat(delay-tweet): 本番環境のmongodbに接続できるように

--- packages/delay-tweet/server/src/models/mongo.ts
+++ packages/delay-tweet/server/src/models/mongo.ts
@@ -7,7 +7,9 @@ const username = process.env.MONGO_DELAY_TWEET_USERNAME;
 const pass = process.env.MONGO_DELAY_TWEET_PASSWORD;
 const dbName = 'delayTweet';
 const auth = (username && pass) ? `${username}:${pass}@` : '';
-const url = `mongodb://${auth}mongodb:27017/${dbName}`;
+log('NODE_ENV: ', process.env.NODE_ENV);
+const url = process.env.NODE_ENV === 'development' ? `mongodb://${auth}mongodb:27017/${dbName}` : 'mongodb+srv://admin:4N6A1d4TlK2gnPm9toOk@delay-tweet-kmxjm.gcp.mongodb.net/test?retryWrites=true&w=majority';
+log('url: ', url);
 log('mongodb url: ', url);
 const mongoOptions = {
   useNewUrlParser: true,
@@ -18,7 +20,7 @@ mongoose.connect(url, mongoOptions).catch(err=>log('connection error'));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console));
 db.once('open', () => {
-  log('connected!');
+  log(`connected to ${url}`);
 });
 
 export {mongoose};
