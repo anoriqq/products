44155fd9194c98b5f0200d5032a486cec0a9a3bd
anoriqq <shota.yoshikawa@anoriqq.com>
2019-12-27 15:41:55 +0900

    chore(youtube-comments): init

---
+++ packages/youtube-comments/server/src/index.ts
@@ -0,0 +1,34 @@
+import http from 'http';
+import express from 'express';
+import helmet from 'helmet';
+import debug from 'debug';
+import morgan from 'morgan';
+
+import { router } from './router';
+
+// Logger
+const log = debug('app:server');
+const requestLogger = debug('request');
+
+// App setup
+const app = express();
+app.set('name', 'niji-timestamp');
+app.set('port', process.env.PORT || 8000);
+app.set('view engine', 'pug');
+app.set('views', `${process.env.PRODUCT_DIR}/server/view`);
+
+// Middleware
+app.use(helmet());
+app.use(morgan('dev', {stream: {write: msg => requestLogger(msg.trimEnd())}}));
+app.use(express.static(`${process.env.PRODUCT_DIR}/client/public`));
+app.use(express.static(`${process.env.PRODUCT_DIR}/client/dist`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Router
+app.use(router);
+
+// Listen
+http.createServer(app).listen(app.get('port'), '0.0.0.0', () => {
+  return log(`${app.get('name')} server is listening on ${app.get('port')}`);
+});
