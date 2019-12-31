0e76e5c293c8708802caa11fe5864b47e5dff703
anoriqq <shota.yoshikawa@anoriqq.com>
2019-12-31 22:59:41 +0900

    feat(mongodb): init時ではなく､起動時に別コンテナからユーザーを作成するように変更

---
+++ packages/mongodb/createMongodbUser.js
@@ -0,0 +1,19 @@
+db.getSiblingDB('delay-tweet').createUser({
+  user: 'delay-tweet',
+  pwd: 'delay-tweet',
+  roles: [{
+    db: 'delay-tweet',
+    role: 'readWrite',
+  }],
+});
+db.getSiblingDB('delay-tweet').createCollection('test');
+
+db.getSiblingDB('youtube-comments').createUser({
+  user: 'youtube-comments',
+  pwd: 'youtube-comments',
+  roles: [{
+    db: 'youtube-comments',
+    role: 'readWrite',
+  }],
+});
+db.getSiblingDB('youtube-comments').createCollection('test');
